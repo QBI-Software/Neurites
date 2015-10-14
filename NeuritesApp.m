04e7e92e97efa95d0dcd448f09264362cd709bc6
lizcw <2015C00per64!>
2015-10-14 14:49:28 +1000

    Help info updated

--- NeuritesApp.m
+++ NeuritesApp.m
@@ -9,7 +9,7 @@
 %       eg. DSdata.csv and SBACdata.csv
 %
 % Output:
-%   1. Neurites_data.csv (analysis)
+%   1. Neurites_data.csv (analysis) and Neurites_data_review.csv (reviewed)
 %   2. Neurites_roi.tif (ROI mask image)
 %   3. Neurites_config.csv (Configuration for overlay of CSV data)
 % 
@@ -20,25 +20,33 @@
 % Usage: 1. Load image file (tif or jpg)
 %       2. Load analysis files (csv) as above
 %       3. Set ROI with ROI tool (radio button in Actions->Image view) -
-%       double-click to save
-%       4. Register CSV overlay - adjust with Configuration options then 
-%   click on 'Save config' this will reload when analysis files are loaded:
+%       double-click to save or load from previous run via File menu
+%       4. Register CSV overlay: plots data from csv files onto image. This
+%           needs to be fitted via following Configuration variables:
 %           Scale: integer (eg 1, 10)
-%           Fit tolerance: Number of pixels either side - usually 1
+%           Fit tolerance: Number of pixels either side. 
+%               Also used in detecting duplicate synapses (default 10)
 %           ShiftX: Number of pixels to shift along x-axis (can be
-%           determined with 'Image Tool -> pixel region button or
-%           measurement tool')
+%               determined with 'Image Tool -> pixel region button or
+%               measurement tool')
 %           ShiftY: Number of pixels to shift along y-axis (as for x)
 %           Min length: Minimum length of a synaptic region (eg 10 pixels)
 %           Tolerance: Nearest neighbour matching of apposing regions (eg 1
 %           pixel)
-%       5. Identify Synaptic Regions - runs analysis. Check results in windows and table.
+%           - 'Save config' this will then reload automatically when CSV 
+%           analysis files are loaded or load a file via File menu
+%       5. Run analysis: Check results in windows and table.
+%       6. Review: Allows examination of each synapse with paths to soma
+%       and end of neurite.  Can remove erroneous branches and delete
+%       duplicate synapses.  This is also good for printing.
+%       7. Compass: Plots angles for each cell (direct line from synapse to
+%       soma centroid)
 %
 % NB. Output files will be overwritten on each run - to save just copy file
 % directly
 % 
 % Developed by Liz Cooper-Williams, QBI (e.cooperwilliams@uq.edu.au)
-% Version: 1.0 (8 Sep 2015)
+% Version: 2.0 (14 Oct 2015)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
