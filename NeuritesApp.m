13b84fb0e27ffef9e961d5c4c00f0d278a806c8a
lizcw <2015C00per64!>
2015-10-15 10:21:14 +1000

    Added description of analysis

--- NeuritesApp.m
+++ NeuritesApp.m
@@ -1,13 +1,35 @@
 % QBI Williams Neurites Synapse Analyser
 % 
-% Description: Developed for Simon de Croft (Williams lab) for the 
-% detection and analysis of synaptic regions of two neurons.
+% Aim: Developed for the detection and analysis of synaptic regions of 
+%   two overlapping neurons.
+% 
+%   
 % Input: 
 %    1. Image (tiff or jpg) with both neurons as two color tracing (from Neurolucida)
 %    2. CSV analysis files for each cell 
 %       ie., Tab 2 'Segment Points to Dendrites') labelled accordingly 
 %       eg. DSdata.csv and SBACdata.csv
-%
+% Description of analysis:
+%    A single Neurolucida drawn image of two neurons is split based on color 
+%   (detected by a single color per neuron only). The images are converted
+%   to binary formats for analysis.  Boundary analysis from the Matlab 
+%   Image Processing Toolbox is used to detect neurites within a manually
+%   drawn ROI (saved as an image mask).  Each boundary segment is matched
+%   against corresponding segments from the other cell and matching regions
+%   (within a configurable tolerance) are detected as synaptic regions.
+%   Data from Neurolucida measurements are used to populate metadata of the
+%   synaptic regions.  The data (referred to as CSV data) is initially overlaid on
+%   the image to obtain the scaling parameters.  Each synaptic
+%   region is referenced to the CSV data to obtain distance and neurite
+%   length measurements.  Duplicate synaptic regions are determined via the
+%   range configured by the fit parameter.
+%   Review of the detected synaptic regions displays the CSV data along
+%   the image neurites.  The centroid of the somas of each cell is detected
+%   via boundary and regionproperty analysis from the Matlab Image
+%   Processing Toolbox and Matlab Statistics Toolbox.  The polar
+%   coordinates of the medians of each cell's synaptic regions are
+%   calculated relevant to that cell's soma centroid and mapped on a
+%   compass map.
 % Output:
 %   1. Neurites_data.csv (analysis) and Neurites_data_review.csv (reviewed)
 %   2. Neurites_roi.tif (ROI mask image)
