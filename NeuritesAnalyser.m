21c75d073e9ad800458a55b3f150741af782404b
lizcw <2015C00per64!>
2015-11-06 10:44:12 +1000

    Flipped radians

--- NeuritesAnalyser.m
+++ NeuritesAnalyser.m
@@ -461,9 +461,9 @@ end
 function [theta,rho,deg] = findAngleSoma(x,y,soma,scale)
         dx = x-soma.centroid(:,1); %x1-x0
         dy = y-soma.centroid(:,2); %y1-y0
-        theta= atan2(dy,dx); %radians
+        theta= atan2(dy,dx) - pi; %radians flipped by 180o == pi
         rho = sqrt(dx.^2 + dy.^2)/scale;
-        deg = radtodeg(theta); %degrees
+        deg = radtodeg(theta); 
         if (deg < 0)
             deg = deg + 360; %negatives
         end
