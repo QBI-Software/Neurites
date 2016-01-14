5629cc33034b82483c7ee8262dffb9fe03a6b526
lizcw <2015C00per64!>
2016-01-14 17:02:54 +1000

    Changes to findAngleSoma in NeuritesAnalyser

--- NeuritesAnalyser.m
+++ NeuritesAnalyser.m
@@ -498,11 +498,11 @@ end
 function [theta,rho,deg] = findAngleSoma(x,y,soma,scale)
         dx = x-soma.centroid(:,1); %x1-x0
         dy = y-soma.centroid(:,2); %y1-y0
-        theta= atan2(dy,dx) - pi; %radians flipped by 180o == pi
+        theta= atan2(dy,dx); %radians flipped by 180o == pi
         rho = sqrt(dx.^2 + dy.^2)/scale;
         deg = radtodeg(theta); 
         if (deg < 0)
-            deg = deg + 360; %negatives
+          %  deg = deg + 360; %negatives
         end
 end
 
