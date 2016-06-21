b3e65cd70b2f17f39758059eaed1366ecff044d8
lizcw <2015C00per64!>
2016-06-21 15:38:02 +1000

    branch order incorrect

--- methods/generateTree.m
+++ methods/generateTree.m
@@ -10,32 +10,38 @@ u = unique(CSV.Order); %COUNT NUMBER ep AND bp
 csvidx = find(CSV.Tree ==1); %indices of matching rows
 branches = {}; %cell(t,u);
 blength = 0;
+bcount = 0;
+atypes=[];
 for i=1:length(csvidx)
     order = CSV.Order(i);
     btype = CSV.PointType(i);
     blength = blength + CSV.Length__m_(i);
     if (strcmp(btype, 'CP')==0)
+        
         if (length(branches) >= order)
             branches{order} = cat(1,branches{order},blength);
         else
             branches{order} = [blength];
         end
         blength = 0;
+        bcount = bcount+1;
+        
     end
         
 end
 %load tree vars
 branchdata = [];%[1 2;3 4];
 branchdistances = [];% [290.7;125;324.2;139.7;0]
-for j=[1:2:length(u)]
-    branchdata = cat(1, branchdata,[j j+1])
-    
+for j=[1:2:bcount-1]
+    branchdata = cat(1, branchdata,[j j+1]);    
 end
 
 for j=[length(branches):-1:1]
-    branchdistances= cat(1, branchdistances, sort(cell2mat(branches(j)),'descend'))
+    branchdistances= cat(1, branchdistances, cell2mat(branches(j)));
 end
-    
+%Check lengths
+sprintf('Num branches: %d',length(branches)) 
+sprintf('Num nodes: %d',bcount)
 
 tree = phytree(branchdata,branchdistances)
 names = get(tree,'LeafNames')
